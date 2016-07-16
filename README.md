935f4828f14a9d647fcb6e3764ce7aa10fadb664
mizutombo <thomas.shultz52@gmail.com>
2016-07-16 15:45:46 -0700

    revd JS code to enable functionality for array summation ... created new styleguide.html ... renamed index.css to styleguide.css ... updated README

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * 7/11/2016 // initiated & cloned new repo 'cookie-stand' ... created working branch 'class06'<br/>
 * 7/15/2016 // completed initialization of JS & HTML code and validated basic functionality ... ACP<br/>
 * 7/15/2016 // revised JS code to use proper construction & syntax of object literal notation ... validated code functionality & output ... ACP<br/>
+* 7/16/2016 // revised JS code to enable correct functionality of daily cookies summation per store ... created new file styleguide.html, and renamed index.css to styleguide.css ... ACP<br/>
+*
--- salmon-cookie-track.js
+++ salmon-cookie-track.js
@@ -1,80 +1,105 @@
 
-  var pioneerSquare = {
+  var storeNo1 = {
   StoreID:            'Pioneer Square',
   MinCust:            17,
   MaxCust:            88,
-  AvgCookiePerSale:   5.2,
+  AvgCookiePerSale:  5.2,
+  CookiePile:          0,
   CookiesSoldHourly: function() {
-  var time = ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '];
-  for (var i = 0; i < time.length; i++) {
-    var cookiesSoldHourly = Math.floor(((Math.random() * (this.MaxCust - this.MinCust + 1)) + this.MinCust) * this.AvgCookiePerSale);
-    console.log(cookiesSoldHourly);
-    document.getElementById('cookie-stand1').innerHTML = this.StoreID;
-    document.getElementById('cookie-stand2').innerHTML += '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    var time = ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '];
+    for (var i = 0; i < time.length; i++) {
+      var cookiesSoldHourly = Math.floor(((Math.random() * (this.MaxCust - this.MinCust + 1)) + this.MinCust) * this.AvgCookiePerSale);
+      this.CookiePile += cookiesSoldHourly;
+      console.log(cookiesSoldHourly);
+      document.getElementById('cookie-stand1').innerHTML = this.StoreID;
+      var salesList = document.getElementById('cookie-stand2');
+      var hourlySales = '<li>'+time[i]+cookiesSoldHourly+'</li>';
+      salesList.innerHTML += hourlySales;
+      document.getElementById('cookie-stand3').innerHTML = '<li>'+'Total: '+this.CookiePile+'</li>';
   }
   }
   };
-  var portlandAirport = {
+  var storeNo2 = {
   StoreID:            'Portland Airport',
-MinCust:              6,
+  MinCust:             6,
   MaxCust:            24,
-  AvgCookiePerSale:   1.2,
+  AvgCookiePerSale:  1.2,
+  CookiePile:          0,
   CookiesSoldHourly: function() {
   var time = ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '];
   for (var i = 0; i < time.length; i++) {
     var cookiesSoldHourly = Math.floor(((Math.random() * (this.MaxCust - this.MinCust + 1)) + this.MinCust) * this.AvgCookiePerSale);
+    this.CookiePile += cookiesSoldHourly;
     console.log(cookiesSoldHourly);
-    document.getElementById('cookie-stand3').innerHTML = this.StoreID;
-    document.getElementById('cookie-stand4').innerHTML += '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    document.getElementById('cookie-stand4').innerHTML = this.StoreID;
+    var salesList = document.getElementById('cookie-stand5');
+    var hourlySales = '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    salesList.innerHTML += hourlySales;
+    document.getElementById('cookie-stand6').innerHTML = '<li>'+'Total: '+this.CookiePile+'</li>';
   }
   }
   };
-  var washingtonSquare = {
+  var storeNo3 = {
   StoreID:            'Washington Square',
   MinCust:            11,
   MaxCust:            38,
-  AvgCookiePerSale:   1.9,
+  AvgCookiePerSale:  1.9,
+  CookiePile:          0,
   CookiesSoldHourly: function() {
   var time = ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '];
   for (var i = 0; i < time.length; i++) {
     var cookiesSoldHourly = Math.floor(((Math.random() * (this.MaxCust - this.MinCust + 1)) + this.MinCust) * this.AvgCookiePerSale);
+    this.CookiePile += cookiesSoldHourly;
     console.log(cookiesSoldHourly);
-    document.getElementById('cookie-stand5').innerHTML = this.StoreID;
-    document.getElementById('cookie-stand6').innerHTML += '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    document.getElementById('cookie-stand7').innerHTML = this.StoreID;
+    var salesList = document.getElementById('cookie-stand8');
+    var hourlySales = '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    salesList.innerHTML += hourlySales;
+    document.getElementById('cookie-stand9').innerHTML = '<li>'+'Total: '+this.CookiePile+'</li>';
   }
   }
   };
-  var sellwood = {
+  var storeNo4 = {
   StoreID:            'Sellwood',
   MinCust:            20,
   MaxCust:            48,
-  AvgCookiePerSale:   3.3,
+  AvgCookiePerSale:  3.3,
+  CookiePile:          0,
   CookiesSoldHourly: function() {
   var time = ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '];
   for (var i = 0; i < time.length; i++) {
     var cookiesSoldHourly = Math.floor(((Math.random() * (this.MaxCust - this.MinCust + 1)) + this.MinCust) * this.AvgCookiePerSale);
+    this.CookiePile += cookiesSoldHourly;
     console.log(cookiesSoldHourly);
-    document.getElementById('cookie-stand7').innerHTML = this.StoreID;
-    document.getElementById('cookie-stand8').innerHTML += '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    document.getElementById('cookie-stand10').innerHTML = this.StoreID;
+    var salesList = document.getElementById('cookie-stand11');
+    var hourlySales = '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    salesList.innerHTML += hourlySales;
+    document.getElementById('cookie-stand12').innerHTML = '<li>'+'Total: '+this.CookiePile+'</li>';
   }
   }
   };
-  var pearlDistrict = {
+  var storeNo5 = {
   StoreID:            'Pearl District',
   MinCust:            3,
   MaxCust:            24,
-  AvgCookiePerSale:   2.6,
+  AvgCookiePerSale:  2.6,
+  CookiePile:          0,
   CookiesSoldHourly: function() {
   var time = ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '];
   for (var i = 0; i < time.length; i++) {
     var cookiesSoldHourly = Math.floor(((Math.random() * (this.MaxCust - this.MinCust + 1)) + this.MinCust) * this.AvgCookiePerSale);
+    this.CookiePile += cookiesSoldHourly;
     console.log(cookiesSoldHourly);
-    document.getElementById('cookie-stand9').innerHTML = this.StoreID;
-    document.getElementById('cookie-stand10').innerHTML += '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    document.getElementById('cookie-stand13').innerHTML = this.StoreID;
+    var salesList = document.getElementById('cookie-stand14');
+    var hourlySales = '<li>'+time[i]+cookiesSoldHourly+'</li>';
+    salesList.innerHTML += hourlySales;
+    document.getElementById('cookie-stand15').innerHTML = '<li>'+'Total: '+this.CookiePile+'</li>';
   }
   }
   };
-  var store = [pioneerSquare, portlandAirport, washingtonSquare, sellwood, pearlDistrict];
+  var store = [storeNo1, storeNo2, storeNo3, storeNo4, storeNo5];
   for (var i = 0; i < store.length; i++) {
     var hourlyCookies = store[i];
     hourlyCookies.CookiesSoldHourly();
