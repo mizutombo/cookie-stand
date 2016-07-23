ad9019f3213a1d4e47d3b98743147d194b25b583
mizutombo <thomas.shultz52@gmail.com>
2016-07-22 23:15:27 -0700

    further refined JS code with nested -for- loops per DRY principles ... debugged code to eliminate NaN error in tally of total cookie sales ... updated READMEs

--- salmon-cookie-objconst.js
+++ salmon-cookie-objconst.js
@@ -9,9 +9,10 @@
     this.cookiesSoldHourly = function() {
       return Math.floor(((Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust) * this.avgCookiePerSale);
       console.log(this.cookiesSoldHourly);
+          var storeHourlySales = this.cookiesSoldHourly();
+          this.cookiePile += storeHourlySales;
     };
   }
-
   var cookieStores = [
     new Store('Pioneer Square', ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '], 17, 88, 5.2),
     new Store('Portland Airport', ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '], 6, 24, 1.2),
@@ -19,51 +20,70 @@
     new Store('Sellwood', ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '], 20, 48, 3.3),
     new Store('Pearl District', ['10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: '], 3, 24, 2.6),
   ];
-
   for (var index = 0; index < cookieStores.length; index++) {
     var currentStore = cookieStores[index];
-    var storeHourlySales = currentStore.cookiesSoldHourly();
+    // initialize var cookiePile
+    var cookiePile = 0;
     // create new row to append to table
     var cookieStoreRow = document.createElement('tr');
     // create row data cell for store name
     var storeNameCell = document.createElement('td');
     storeNameCell.innerText = currentStore.storeName;
     cookieStoreRow.appendChild(storeNameCell);
-    // create row data cells for cookies sold each hour throughout the day ... run function for cookies sold hourly, using random number engine
-    var storeHourlyCookiesCell10am = document.createElement('td');
-    storeHourlyCookiesCell10am.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell10am);
-    var storeHourlyCookiesCell11am = document.createElement('td');
-    storeHourlyCookiesCell11am.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell11am);
-    var storeHourlyCookiesCell12pm = document.createElement('td');
-    storeHourlyCookiesCell12pm.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell12pm);
-    var storeHourlyCookiesCell1pm = document.createElement('td');
-    storeHourlyCookiesCell1pm.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell1pm);
-    var storeHourlyCookiesCell2pm = document.createElement('td');
-    storeHourlyCookiesCell2pm.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell2pm);
-    var storeHourlyCookiesCell3pm = document.createElement('td');
-    storeHourlyCookiesCell3pm.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell3pm);
-    var storeHourlyCookiesCell4pm = document.createElement('td');
-    storeHourlyCookiesCell4pm.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell4pm);
-    var storeHourlyCookiesCell5pm = document.createElement('td');
-    storeHourlyCookiesCell5pm.innerText = currentStore.cookiesSoldHourly();
-    cookieStoreRow.appendChild(storeHourlyCookiesCell5pm);
-    // tally up total hourly cookies for this store
-    currentStore.cookiePile += storeHourlySales;
-    // create row data cell for tally of daily cookie sales for this store
-    var storeTotalSales = document.createElement('td');
-    storeTotalSales.innerText = currentStore.cookiePile;
-    cookieStoreRow.appendChild(storeTotalSales);
+    for (var i = 0; i < currentStore.storeTime.length; i++) {
+      // create row data cells for cookies sold each hour throughout the day ... run function for cookies sold hourly, using random number engine
+      var storeHourlyCookiesCell = document.createElement('td');
+      storeHourlyCookiesCell.innerText = currentStore.cookiesSoldHourly();
+      // keep running tally of hourly cookie sales for store
+      currentStore.cookiePile += parseInt(storeHourlyCookiesCell.innerText);
+      console.log(storeHourlyCookiesCell.innerText);
+      console.log(currentStore.cookiePile);
+      cookieStoreRow.appendChild(storeHourlyCookiesCell);
+    }
+    // create row data cell for total daily cookie sales for store
+    var storeTotalSalesCell = document.createElement('td');
+    storeTotalSalesCell.innerText = currentStore.cookiePile;
+    console.log(storeTotalSalesCell.innerText);
+    cookieStoreRow.appendChild(storeTotalSalesCell);
     // append new row onto table
     var table = document.getElementById('cookie-wookie');
     table.appendChild(cookieStoreRow);
   }
+
+    // var storeHourlyCookiesCell10am = document.createElement('td');
+    // storeHourlyCookiesCell10am.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell10am);
+    // var storeHourlyCookiesCell11am = document.createElement('td');
+    // storeHourlyCookiesCell11am.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell11am);
+    // var storeHourlyCookiesCell12pm = document.createElement('td');
+    // storeHourlyCookiesCell12pm.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell12pm);
+    // var storeHourlyCookiesCell1pm = document.createElement('td');
+    // storeHourlyCookiesCell1pm.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell1pm);
+    // var storeHourlyCookiesCell2pm = document.createElement('td');
+    // storeHourlyCookiesCell2pm.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell2pm);
+    // var storeHourlyCookiesCell3pm = document.createElement('td');
+    // storeHourlyCookiesCell3pm.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell3pm);
+    // var storeHourlyCookiesCell4pm = document.createElement('td');
+    // storeHourlyCookiesCell4pm.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell4pm);
+    // var storeHourlyCookiesCell5pm = document.createElement('td');
+    // storeHourlyCookiesCell5pm.innerText = currentStore.cookiesSoldHourly();
+    // cookieStoreRow.appendChild(storeHourlyCookiesCell5pm);
+    // tally up total hourly cookies for this store
+    // currentStore.cookiePile += storeHourlySales;
+    // create row data cell for tally of daily cookie sales for this store
+  //   var storeTotalSales = document.createElement('td');
+  //   storeTotalSales.innerText = currentStore.cookiePile;
+  //   cookieStoreRow.appendChild(storeTotalSales);
+  //   // append new row onto table
+  //   var table = document.getElementById('cookie-wookie');
+  //   table.appendChild(cookieStoreRow);
+  // }
 
         // for (var index = 0; index < cookieStores.length; index++) {
         //   var currentStore = cookieStores[index];
